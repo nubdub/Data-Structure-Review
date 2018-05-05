63368874ebf6762a25b35e376959b4adcaa5f3f5
Nabdeep Shrestha <nabdeep@vt.edu>
2018-05-05 16:51:28 -0400

    Finished ArrayBag

--- src/nubdub/ArrayBag.java
+++ src/nubdub/ArrayBag.java
@@ -8,74 +8,165 @@ package nubdub;
  *
  */
 public class ArrayBag<T> implements BagInterface<T> {
-    
+
     private T[] bag;
     private int numberOfEntries;
     private int capacity;
     private static final int DEFAULT_CAPACITY = 25;
-    
+
+
+    /**
+     * Initializes an ArrayBag with a default capacity
+     */
     public ArrayBag() {
         this(DEFAULT_CAPACITY);
     }
-    
+
+
+    /**
+     * Initializes fields for ArrayBag
+     * 
+     * @param capacity
+     *            The desired capacity of the ArrayBag
+     */
     public ArrayBag(int capacity) {
+        initializeDataFields(capacity);
+    }
+
+
+    /**
+     * @param capacity
+     */
+    private void initializeDataFields(int capacity) {
         @SuppressWarnings("unchecked")
         T[] tempBag = (T[])new Object[capacity];
         bag = tempBag;
+        this.capacity = capacity;
         numberOfEntries = 0;
     }
 
+    /**
+     * Determines if bag is empty
+     */
     @Override
     public boolean isEmpty() {
         return numberOfEntries == 0;
     }
 
+
     @Override
     public int getCurrentSize() {
-        // TODO Auto-generated method stub
-        return 0;
+        return numberOfEntries;
     }
 
+
+    /**
+     * Adds an object to the bag
+     * 
+     * @param entry
+     *            The object to be added to the bag
+     * 
+     * @return True if the addition was successful
+     */
     @Override
     public boolean add(T entry) {
-        // TODO Auto-generated method stub
-        return false;
+        if (numberOfEntries == capacity) {
+            return false;
+        }
+        else {
+            bag[numberOfEntries] = entry;
+            numberOfEntries++;
+            return true;
+        }
     }
 
+    /**
+     * Clears the bag
+     */
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        
+        initializeDataFields(capacity);
     }
 
+    /**
+     * Removes an entry from the bag
+     * 
+     * @return The removed entry
+     */
     @Override
     public T remove() {
-        // TODO Auto-generated method stub
-        return null;
+        T removed = null;
+        if (numberOfEntries > 0) {
+            removed = bag[numberOfEntries - 1];
+            bag[numberOfEntries - 1] = null;
+            numberOfEntries--;
+        }
+        return removed;
     }
 
+    /**
+     * Removes the given entry if it's in the bag
+     */
     @Override
     public boolean remove(T entry) {
-        // TODO Auto-generated method stub
-        return false;
+        if (!contains(entry)) {
+            return false;
+        }
+        else {
+            int index = getIndexOf(entry);
+            bag[index] = bag[numberOfEntries - 1];
+            bag[numberOfEntries - 1] = null;
+            numberOfEntries--;
+            return true;
+        }
     }
 
+    /**
+     * Gets the number of occurrences of an object in the bag
+     */
     @Override
     public int getFrequencyOf(T entry) {
-        // TODO Auto-generated method stub
-        return 0;
+        int counter = 0;
+        for (int i = 0; i < bag.length; i++) {
+            if (entry.equals(bag[i])) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
+    /**
+     * Checks if the entry is in the bag
+     */
     @Override
     public boolean contains(T entry) {
-        // TODO Auto-generated method stub
-        return false;
+        boolean found = false;
+        for (int i = 0; i < bag.length; i++) {
+            if (entry.equals(bag[i])) {
+                found = true;
+                return found;
+            }
+        }
+        return found;
     }
+
 
     @Override
     public T[] toArray() {
-        // TODO Auto-generated method stub
-        return null;
+        return bag;
+    }
+    
+    private int getIndexOf(T entry) {
+        int result = -1;
+        if (contains(entry)) {
+            for (int i = 0; i < bag.length; i++) {
+                if (entry.equals(bag[i])) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
 }
